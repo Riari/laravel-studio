e84ae8fed00c486cba12c56dd014e8beb7b54bde
Riari <hello@ricko.me>
2016-09-06 23:45:30 +0100

    Add laravel-forum & laravel-forum-frontend

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Riari\Forum\ForumServiceProvider::class,
+        Riari\Forum\ForumServiceProvider::class,
+        Riari\Forum\Frontend\ForumFrontendServiceProvider::class,
 
     ],
 
