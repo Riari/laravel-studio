32ccfc249f260546fdd636d34f1812722c91b15f
Riari <3583774+Riari@users.noreply.github.com>
2018-11-05 22:25:40 +0000

    Add forum link

--- code/resources/views/welcome.blade.php
+++ code/resources/views/welcome.blade.php
@@ -66,12 +66,11 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
+            <div class="top-right links">
+                <a href="{{ url('/forum') }}">Forum</a>
+                <a href="{{ url('/login') }}">Login</a>
+                <a href="{{ url('/register') }}">Register</a>
+            </div>
 
             <div class="content">
                 <div class="title m-b-md">
